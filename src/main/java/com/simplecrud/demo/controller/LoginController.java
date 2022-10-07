d7bee6ceee81dc72a363ac2251916ee12e7d202a
ozosena <laracardozojuansebastian@gmail.com>
2022-10-06 21:29:13 -0500

    login

--- src/main/java/com/simplecrud/demo/controller/LoginController.java
+++ src/main/java/com/simplecrud/demo/controller/LoginController.java
@@ -1,7 +1,5 @@
 package com.simplecrud.demo.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @GetMapping("")
-    public ResponseEntity holamundo() {
-        return new ResponseEntity("ok", HttpStatus.OK);
+    public String holamundo() {
+        return "login";
     }
 
 }
-
-//localhost:8080/acacadawdawd
